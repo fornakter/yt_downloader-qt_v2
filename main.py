a2b0f92cb679a4908689d8a76c19e1e9309a3cee
fornakter <fornakter@gmail.com>
2025-06-03 22:07:22 +0200

    0.7

--- main.py
+++ main.py
@@ -1,67 +1,121 @@
-
 from PyQt5 import QtCore, QtGui, QtWidgets
 import yt_dlp
 import threading, requests
+import os
+
+from PyQt5.QtWidgets import QMessageBox
 
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(645, 119)
+        MainWindow.resize(645, 120)
+        MainWindow.setMinimumSize(QtCore.QSize(645, 120))
+        MainWindow.setMaximumSize(QtCore.QSize(645, 120))
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-
-        # Url window
         self.UrlEdit = QtWidgets.QLineEdit(self.centralwidget)
         self.UrlEdit.setGeometry(QtCore.QRect(10, 10, 511, 27))
+        self.UrlEdit.setPlaceholderText("")
         self.UrlEdit.setObjectName("UrlEdit")
-        self.UrlEdit.setText("https://www.youtube.com/watch?v=0qIVDMvNx2Y")
 
         # Go button
+
         self.goButton = QtWidgets.QPushButton(self.centralwidget)
         self.goButton.setGeometry(QtCore.QRect(540, 10, 94, 27))
         self.goButton.setObjectName("goButton")
         self.goButton.clicked.connect(self.goButtonClick)
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(20, 60, 71, 19))
+        self.label.setGeometry(QtCore.QRect(20, 90, 71, 19))
         self.label.setObjectName("label")
 
-        # Status label
+        # Status Text
+
         self.StatusText = QtWidgets.QLabel(self.centralwidget)
-        self.StatusText.setGeometry(QtCore.QRect(70, 60, 71, 19))
+        self.StatusText.setGeometry(QtCore.QRect(70, 90, 451, 19))
+        self.StatusText.setToolTip("")
         self.StatusText.setObjectName("StatusText")
+
+        # Info Button
+
         self.InfoButton = QtWidgets.QPushButton(self.centralwidget)
         self.InfoButton.setGeometry(QtCore.QRect(540, 50, 94, 27))
         self.InfoButton.setObjectName("InfoButton")
+        self.InfoButton.clicked.connect(self.infoButtonClick)
+
+        # Radio button Audio
+
+        self.radioButton = QtWidgets.QRadioButton(self.centralwidget)
+        self.radioButton.setGeometry(QtCore.QRect(20, 50, 117, 25))
+        self.radioButton.setChecked(True)
+        self.radioButton.setObjectName("radioButton")
+
+        # Radio button Video
+
+        self.radioButton_2 = QtWidgets.QRadioButton(self.centralwidget)
+        self.radioButton_2.setGeometry(QtCore.QRect(150, 50, 117, 25))
+        self.radioButton_2.setObjectName("radioButton_2")
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
+    def infoButtonClick(self):
+        msg = QMessageBox()
+        msg.setWindowTitle("info")
+        msg.setText("\n   Pobieraj filmy i muzykę z YT   \n"
+                    "   Autor: Adam Fatyga\n"
+                    "   Wersja 0.7\n")
+
+        x = msg.exec_()
+
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "yt_dl"))
         self.goButton.setText(_translate("MainWindow", "Go!"))
         self.label.setText(_translate("MainWindow", "Status: "))
         self.StatusText.setText(_translate("MainWindow", "TextLabel"))
         self.InfoButton.setText(_translate("MainWindow", "Info"))
+        self.radioButton.setText(_translate("MainWindow", "Audio"))
+        self.radioButton_2.setText(_translate("MainWindow", "Video"))
+
 
     # Download file
-    def download_file(self):
+    def download_file(self, output_path='./'):
         if self.check_net():
 
-            try:
+            # audio
+            if self.radioButton.isChecked():
                 ydl_opts = {
-                    'format': 'best',  # Pobierz najlepszą jakość
-                    'outtmpl': '%(title)s.%(ext)s',  # Nazwa pliku
+                    'format': 'bestaudio/best',
+                    'postprocessors': [{
+                        'key': 'FFmpegExtractAudio',
+                        'preferredcodec': 'mp3',
+                        'preferredquality': '256',
+                    }],
+
+                    'outtmpl': os.path.join(output_path, '%(title)s.%(ext)s'),
+                }
+            else:
+                print("video")
+
+                # video
+
+                ydl_opts = {
+                    'format': 'bestvideo+bestaudio/best',  # pobiera najlepszą kombinację wideo+audio
+                    'outtmpl': os.path.join(output_path, '%(title)s.%(ext)s'),
                 }
 
+            try:
                 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                    ydl.download([self.UrlEdit.text()])
-                self.goButton.setEnabled(True)
+                    info = ydl.extract_info(self.UrlEdit.text(), download=True)
+                    print(f"Downloaded: {info.get('title', 'unknown title')}")
                 self.StatusText.setText("Done!")
             except Exception as e:
                 self.StatusText.setText(str(e))
+            self.goButton.setEnabled(True)
+
+
 
     # Go button action
     def goButtonClick(self):
